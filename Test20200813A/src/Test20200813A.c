17ac1e0d307be1388aea9f3ccec24dfdb6d9275c
KR001 <vf3k-tkuc@asahi-net.or.jp>
2021-02-09 22:02:11 -0500

    Initial Commit

---
+++ Test20200813A/src/Test20200813A.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : Test20200813A.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!\n"); /* prints !!!Hello World!!! */
+	puts("Change after the project inported to Git"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
