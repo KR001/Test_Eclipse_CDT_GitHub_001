a5f6432fe7d9f32daf470c71ed608998d83578d8
KR001 <vf3k-tkuc@asahi-net.or.jp>
2021-02-11 20:17:14 -0500

    Cloned from GITHUB to Yoga940 and committing and pushing back to github, again.

--- Test20200813A/src/Test20200813A.c
+++ Test20200813A/src/Test20200813A.c
@@ -14,5 +14,6 @@
 int main(void) {
 	puts("!!!Hello World!!!\n"); /* prints !!!Hello World!!! */
 	puts("Change after the project imported to Git"); /* prints !!!Hello World!!! */
+	puts("Change after cloned from GITHUB"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
